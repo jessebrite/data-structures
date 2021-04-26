af80ce4068440aac4574a0170a09cc6c7d49a268
jessebrite <aboagye@protonmail.com>
2021-04-27 00:58:19 +0100

    working as expected

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,13 @@ public class Main {
 	public static void main(String[] args) {
 		Stack<Integer> stack = new StackArray<>();
 
+		System.out.println(stack);
+
 		stack.push(10);
 		stack.push(30);
 		stack.push(50);
 		stack.push(40);
+		stack.push(20);
 		System.out.println(stack);
 
 		System.out.println(stack.peek());
