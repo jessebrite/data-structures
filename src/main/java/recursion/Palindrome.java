8da263c95712875ac41e5ec9c759a06834ed50c1
jessebrite <aboagye@protonmail.com>
2021-05-25 22:27:53 +0100

    code clean-up

--- src/main/java/recursion/Palindrome.java
+++ src/main/java/recursion/Palindrome.java
@@ -2,21 +2,15 @@ public class Palindrome {
   /**
    * A recursive function to check if a string is a palindrome
    *
-   * @param phrase
-   * @return boolean
+   * @param phrase This parameter is what will be tested
+   * @return Whether or not the phrase is a palindrome
    */
   public static boolean isPalindrome(String phrase) {
     if (phrase.length() <= 1) return true;
-    // Sheck the first and last characters of the string if they are the same
+    // Compare the first and last characters of the string
     // and recurseively check the subsequent characters.
     // If comparison returns false, it's not a palindrome
     return phrase.charAt(0) == phrase.charAt(phrase.length() - 1)
       && isPalindrome(phrase.substring(1, phrase.length() - 1));
-  }
-
-  public static void main(String[] args) {
-    System.out.println(isPalindrome("a"));
-    System.out.println(isPalindrome("ama"));
-    System.out.println(isPalindrome("ama an na ama"));
   }
 }
