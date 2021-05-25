6b0d12dbaacbaee79968944aade3a2334828855e
jessebrite <aboagye@protonmail.com>
2021-05-25 22:25:55 +0100

    class properly documented

--- src/main/java/recursion/TowersOfHanoi.java
+++ src/main/java/recursion/TowersOfHanoi.java
@@ -1,14 +1,22 @@
 package recursion;
 
 public class TowersOfHanoi {
+/**
+ * This is the classic Towers of Hanoi recursion problem
+ * We are trying to move all disks from source to destination
+ * using an auxiliary location
+ *
+ * @param disk This is the number of disks to move
+ * @param source This is where the disks initially are
+ * @param destination This is where we finally want to send the disks
+ * @param temp This is a temporary holder for moving disks around
+ */
 	public static void towerOfHanoi(int disk, int source, int destination, int temp) {
-//		base case
 //		print from source to destination
 		if (disk == 1) {
 			System.out.printf("%n%d --> %d", source, destination);
 			return;
 		}
-
 //		move disks from src to temp through destination
 		towerOfHanoi(disk - 1, source, temp, destination);
 
